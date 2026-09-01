de9d79d1b332ecedebf9f17f2005423c619dbc62
Ateeksh <ateesood@gmail.com>
2026-09-01 12:59:01 +0000

    added the label session fn to label the session id and pos in session for each request , added the drop robot fn which will drop anyone trynna access robot.txt stuff and wrote drop long sessions which will drop the sessions whose length is more than the one we have specified

--- src/auspex/sessionize.py
+++ src/auspex/sessionize.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from auspex.config import Cfg
@@ -14,18 +15,44 @@ def load(name : str) -> pd.DataFrame:
     return df
 
 
-def label_sessions(df : pd.DataFrame, cfg : Cfg):
+def label_sessions(df : pd.DataFrame, cfg : Cfg) -> pd.DataFrame:
     
     df_sorted = df.sort_values(by = ["host" , "ts" , "seq"] ,  ignore_index=True)
-    host_changed = df_sorted["host"] != df_sorted["host"].shift(1)
-    session_gap = df_sorted["ts"] - df_sorted["ts"].shift(1) > cfg.session_gap_s 
+    host_changed_boolean = df_sorted["host"] != df_sorted["host"].shift(1)
+    session_gap_boolean = df_sorted["ts"] - df_sorted["ts"].shift(1) > cfg.session_gap_s 
    
-    sorted_host_session = host_changed | session_gap
+    sorted_host_session = host_changed_boolean | session_gap_boolean
 
     df_sorted["session_id"] = sorted_host_session.cumsum()
 
+    df_sorted["pos_in_session"] = df_sorted.groupby("session_id").cumcount() 
+
     return df_sorted
 
+def drop_robots_hosts(df : pd.DataFrame) -> tuple[pd.DataFrame , int]:
+
+    bad_hosts =  df.loc[df["url"] == "/robots.txt" , "host"].unique()
+    mask = df["host"].isin(bad_hosts)
+    rows_dropped = mask.sum()
+    robot_hosts_free_df = df[~mask]
+
+    return (robot_hosts_free_df , rows_dropped)
+
+
+def drop_long_sessions(df : pd.DataFrame, cfg : Cfg) -> tuple[pd.DataFrame , int]:
+
+    session_length = df.groupby("session_id").size()
+
+    mask_length = session_length[session_length > cfg.bot_max_session_len]
+
+    print(f"Maximum sessions in the file : {mask_length.nlargest(5)}")
+
+    mask = df.session_id.isin(mask_length.index)
+    rows_dropped = mask.sum()
+    long_session_free_df = df[~mask]
+
+    return (long_session_free_df , rows_dropped)
+    
     
 
 
@@ -34,6 +61,14 @@ def main():
 
     cfg = Cfg()
     df_sorted = label_sessions(df , cfg )
+    robot_free_df , robot_rows_removed = drop_robots_hosts(df_sorted)
+
+    print(f"Number of robot rows removed are : {robot_rows_removed}" )
+
+    long_session_free_df , long_session_rows_removed = drop_long_sessions(robot_free_df , cfg)
+
+    print(f"Number of long session rows removed are : {long_session_rows_removed}" )
+
     
 
 if __name__ == "__main__":
