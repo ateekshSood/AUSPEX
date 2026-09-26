bd32d20d1630fc0baa6cf0c3be1d26a4f29d2e35
Ateeksh <ateesood@gmail.com>
2026-09-26 04:47:40 +0000

    wrote some basic explanaiton of lfu

--- src/auspex/policies/lfu.py
+++ src/auspex/policies/lfu.py
@@ -7,6 +7,28 @@ class LFU(Policy):
 
     name = "lfu"
 
+    ''' 
+    our lfu will have two structures in it for it to work. the first structure is a hash map which is mapped from 
+    
+    structure 1 : (url -> count / num of its occurance )
+    
+    this strucure shows which url has been used how many times hence satisfying the least frequently used format of LFU cache
+    now we also have to evict the urls that have been least used and in case of a tie in count we have to decide somehow 
+    whihc url we need to evict for this we will make our second strucuture 
+
+    strucutre 2 : hashmap : count -> OrderedDict of urls
+
+    now we make another hashmap which has mapping from count to the ordered dict of the urls now why are we using count as key ? 
+    we are doing it for our eviction step we can maintain a min count and then chekc which urls have these counts 
+    now why are we using ordered dict of urls as value , we are doing this cuz say we have multiple urls which has same count 
+    so how do we decide which url we have to evict, for this we are using ordered dict , in whihc the most recently used urls 
+    are kept at back and the least recently used urls are kept at first hence providing us with an easy way of eviction
+
+    when we have to perform get() on a url we have to chnage its count and from that we can change its posiiton in structure 2
+    for that we have to maintain structure 1.
+    
+    '''
+
     def __init__(self , capacity : int):
         super().__init__(capacity)
         self.url_count_map_structure = {}
